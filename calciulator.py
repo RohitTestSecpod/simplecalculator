83d790807e6350044106a211d7c5da1e3efbc6a4
RohitTestSecpod <rohittestsecpod@gmail.com>
2024-08-08 13:16:16 +0530

    Update calciulator.py

--- calciulator.py
+++ calciulator.py
@@ -1,55 +1,22 @@
-# This function adds two numbers
-def add(x, y):
-    return x + y
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
 
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
 
-# This function divides two numbers
-def divide(x, y):
-    return x / y
+# main driver function
+if __name__ == '__main__':
 
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input")
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
